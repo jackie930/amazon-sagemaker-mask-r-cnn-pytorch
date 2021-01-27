6314d1f53cf059efbbe55e24f9684f954107738a
jackie <jackieliu_cornell@163.com>
2021-01-27 14:36:47 +0800

    update preprocessing file

--- container/mask_r_cnn/data_prepare.py
+++ container/mask_r_cnn/data_prepare.py
@@ -39,8 +39,81 @@ def prepare(labelfile,target_folder):
         except:
             continue
 
+def prepare_modelc(labelfile,target_folder):
+    with open(labelfile, "r") as f:
+        annotations = json.load(f, encoding='unicode-escape')
+    annotations_values = list(annotations.values()) # don't need the dict keys
+    print (annotations_values[0])
+    print (len(annotations_values))
+    (filepath, tempfilename) = os.path.split(labelfile)
+    print ("<<< file path: ", filepath)
+    make_dirs(target_folder)
+
+    for i in range(len(annotations_values)):
+        try:
+            if len(annotations_values[i]['regions'])>0:
+                tempfilename = annotations_values[i]['filename']
+                (filename, extension) = os.path.splitext(tempfilename)
+
+                json_name = os.path.join(target_folder,'label',filename+'.json')
+                with open(json_name, 'w') as outfile:
+                    json.dump(annotations_values[i], outfile)
+        except:
+            continue
+
+def prepare_modelc(labelfile,target_folder):
+    with open(labelfile, "r") as f:
+        annotations = json.load(f, encoding='unicode-escape')
+    annotations_values = list(annotations.values()) # don't need the dict keys
+    print (annotations_values[0])
+    print (len(annotations_values))
+    (filepath, tempfilename) = os.path.split(labelfile)
+    print ("<<< file path: ", filepath)
+    make_dirs(target_folder)
+
+    for i in range(len(annotations_values)):
+        try:
+            if len(annotations_values[i]['regions'])>0:
+                tempfilename = annotations_values[i]['filename']
+                (filename, extension) = os.path.splitext(tempfilename)
+
+                json_name = os.path.join(target_folder,'label',filename+'.json')
+                with open(json_name, 'w') as outfile:
+                    json.dump(annotations_values[i], outfile)
+        except:
+            continue
+
+def prepare_modelb(labelfile,target_folder):
+    with open(labelfile, "r") as f:
+        annotations = json.load(f, encoding='unicode-escape')
+    annotations_values = list(annotations.values()) # don't need the dict keys
+    print (annotations_values[0])
+    print (len(annotations_values))
+    #(filepath, tempfilename) = os.path.split(labelfile)
+    #print ("<<< file path: ", filepath)
+    make_dirs(target_folder)
+
+    for i in range(len(annotations_values)):
+        try:
+            if len(annotations_values[i]['regions'])>0:
+                labelfile = annotations_values[i]['filename']
+                (filepath, tempfilename) = os.path.split(labelfile)
+                (filename, extension) = os.path.splitext(tempfilename)
+                json_name = os.path.join(target_folder,'label',filename+'.json')
+                with open(json_name, 'w') as outfile:
+                    json.dump(annotations_values[i], outfile)
+        except:
+            continue
+
 if __name__ == '__main__':
     print (sys.getdefaultencoding())
     target_folder = './res'
-    prepare('./1000b-tag.json',target_folder)
+    #origin 1000
+    #prepare('/Users/liujunyi/Desktop/spottag/customer/联保/1000b-tag.json',target_folder)
+    #modelb
+    prepare_modelb('/Users/liujunyi/Desktop/spottag/customer/联保/label-2/20210125_model_b_dataset/new_label_output.json',
+                   '/Users/liujunyi/Desktop/spottag/customer/联保/label-2/20210125_model_b_dataset')
+
+    #model c
+    #prepare_modelc('/Users/liujunyi/Desktop/spottag/customer/联保/label-2/20210125_image_segmentation/1.json','/Users/liujunyi/Desktop/spottag/customer/联保/label-2/20210125_image_segmentation')
     #make_dirs(target_folder)
